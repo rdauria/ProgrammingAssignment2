c1ad64743b5c667d19d33acde2c7a0341b6f61ba
Raffaella D'Auria <rdauria@users.noreply.github.com>
2015-01-23 16:04:32 -0800

    My cachematrix.R assignment with comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,17 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
+## Write a short comment describing this function:
+## makeCacheMatrix first checks whether the input matrix is square
+## and if its determinant is different than 0 if either of this conditions
+## are not mat it prints a hint and exits. In case the matrix is square and
+## its determinant is different than 0 (an inverse of the matrix exists), 
+## makeCacheMatrix creates a special matrix, a list, which components  
+## are functions that:
+## set the value of the matrix
+## get the value of the matrix
+## set the value of the matrix inverse
+## get the value of the matrix inverse
 makeCacheMatrix <- function(x = matrix()) {
     
     if (dim(x)[1] != dim(x)[2]) {
@@ -30,6 +39,10 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
+## cacheSolve takes the special matrix object created by makeCacheMatrix
+## and checks whether an inverse for the matrix passes as argument to 
+## makeCacheMatrix already exists (checks whether the matrix does not
+## contain NAs) if not it computes the inverse and sets in the cache
 
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
